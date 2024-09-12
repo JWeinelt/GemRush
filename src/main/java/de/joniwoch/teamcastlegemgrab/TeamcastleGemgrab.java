6c5cbd943614cd87c2fd0813ec46d8dea64e2152
Joniwoch <jonathan.wochniks@gmail.com>
2024-09-12 19:57:50 +0200

    init

---
+++ src/main/java/de/joniwoch/teamcastlegemgrab/TeamcastleGemgrab.java
@@ -0,0 +1,17 @@
+package de.joniwoch.teamcastlegemgrab;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeamcastleGemgrab extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
