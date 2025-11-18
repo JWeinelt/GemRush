bd6a832b9577e8d1bcbce3f2b14cec0e1c7fc9f8
Julian Weinelt <julian.weinelt@outlook.de>
2025-11-18 12:09:15 +0100

    improvement: delete redundant calls on join

--- src/main/java/net/teamcastle/gemgrab/listener/JoinListener.java
+++ src/main/java/net/teamcastle/gemgrab/listener/JoinListener.java
@@ -1,58 +1,18 @@
 package net.teamcastle.gemgrab.listener;
 
-import net.teamcastle.gemgrab.TeamcastleGemgrab;
-import net.teamcastle.gemgrab.manager.database.MySQLManager;
-import net.teamcastle.gemgrab.manager.items.lobbyitems.LobbyItemManager;
-import net.teamcastle.gemgrab.manager.locations.LobbyLocationManager;
-import net.teamcastle.gemgrab.manager.player.GemgrabPlayerManager;
-import net.teamcastle.gemgrab.scoreboard.Scoreboard;
-import net.teamcastle.gemgrab.utils.Messages;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.GameMode;
-import org.bukkit.Sound;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import java.sql.SQLException;
-import java.util.Calendar;
-
 @RequiredArgsConstructor
 public class JoinListener implements Listener {
-
-    private final LobbyItemManager lobbyItemManager;
-    private final LobbyLocationManager lobbyLocationManager;
-    private final Scoreboard scoreboard;
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        event.setJoinMessage(Messages.mainPrefix + "§a" + player.getName() + "§7 hat den Server §abetreten§7!");
-        switch (TeamcastleGemgrab.getGamestate()) {
-            case LOBBY -> {
-                doLobbyJoin(player);
-            }
-        }
-    }
-
-    public void doLobbyJoin(Player player) {
-        GemgrabPlayerManager.addGemgrabPlayer(player);
-        lobbyItemManager.setLobbyItems(player);
-        lobbyLocationManager.teleportLobbySpawn(player);
-        player.setGameMode(GameMode.SURVIVAL);
-        player.setAllowFlight(false);
-        player.setHealth(20);
-        player.setFoodLevel(20);
-        player.setLevel(Calendar.getInstance().get(Calendar.YEAR));
-        player.setExp((float) Calendar.getInstance().get(Calendar.DAY_OF_YEAR) / 365);
-        player.sendTitle("§a§lGemGrab", "§aViel Erfolg!");
-        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 3);
-        scoreboard.setScoreboard(player);
-        try {
-            TeamcastleGemgrab.getInstance().getMySQLManager().createPlayer(player.getUniqueId());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        event.joinMessage(Component.empty());
     }
 }
