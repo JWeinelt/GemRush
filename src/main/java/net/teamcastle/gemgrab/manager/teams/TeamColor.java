121ef2a0724fef56c8fa361ea3e89d1535e7704b
Julian Weinelt <julian.weinelt@outlook.de>
2025-11-18 12:14:56 +0100

    api: add necessary values to TeamColor Enum

--- src/main/java/net/teamcastle/gemgrab/manager/teams/TeamColor.java
+++ src/main/java/net/teamcastle/gemgrab/manager/teams/TeamColor.java
@@ -1,7 +1,30 @@
 package net.teamcastle.gemgrab.manager.teams;
 
-public enum TeamColor {
+import net.teamcastle.gemgrab.manager.game.Game;
+import org.bukkit.Material;
 
-    BLUE,
-    RED
+public enum TeamColor {
+    BLUE("§9", "Blue", Material.BLUE_CONCRETE),
+    RED("§c", "Red", Material.RED_CONCRETE),
+    UNKNOWN("§7", "Unknown", Material.GRAY_CONCRETE);
+
+    public final String colorCode;
+    public final String displayName;
+    public final Material material;
+
+    TeamColor(String colorCode, String displayName, Material material) {
+        this.colorCode = colorCode;
+        this.displayName = displayName;
+        this.material = material;
+    }
+
+    public static TeamColor of(String input) {
+        return valueOf(input);
+    }
+
+    public static TeamColor getTeamWithLeastPlayers(Game game) {
+        int bluePlayers = game.getPlayers().get(BLUE).size();
+        int redPlayers = game.getPlayers().get(RED).size();
+        return bluePlayers <= redPlayers ? BLUE : RED;
+    }
 }
